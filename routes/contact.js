06dab89708aa6a5370474a767d564c15f3f72d52
Biman-Mandal <bimanm193@gmail.com>
2023-04-21 15:05:33 +0530

    wp contact route added

--- routes/contact.js
+++ routes/contact.js
@@ -2,15 +2,25 @@ const express = require('express');
 const router  = express.Router();
 const fs      = require('fs');
 const path    = require('path');
+const axios   = require('axios');
 
 router.get('/get', async(req, res)=>{
-    client[req.body.workspace_id].getChats().then((chats)=>{
-        console.log(chats);
-    })
+    client[req.body.workspace_id][req.body.connection_no].getContacts().then((contacts) => {
+            res.send({
+                'status' : 200,
+                'data'   : 'all_contacts',
+                'message': {
+                    'workspace_id'   : req.body.workspace_id,
+                    'connection_no'  : req.body.connection_no,
+                    'contacts' : contacts
+                }
+            });
+        }).catch((error)=>{
+            res.status(500).send({
+                'status' : 'error',
+                'data'   : 'error',
+                'message': 'Error In data save'
+            });
+        });
 });
-
-router.get('/info', async(req, res)=>{
-    res.send(client[req.body.workspace_id].info);
-});
-
 module.exports = router;
